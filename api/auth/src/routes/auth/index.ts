222ad1a03bb7ea011d92ae7931b70878bb5b7b75
uma-uc <uma.apple123@gmail.com>
2025-08-29 19:11:57 -0700

    feat: fix-signup

--- api/auth/src/routes/auth/index.ts
+++ api/auth/src/routes/auth/index.ts
@@ -141,7 +141,7 @@ authRoutes.openapi(signup, async (c) => {
         {
           id: userId,
           email,
-          emailVerifiedAt: envVars.MOCK_DATA ? Date.now().toString() : null,
+          emailVerifiedAt: envVars.MOCK_DATA ? new Date().toISOString() : null,
           isSuperAdmin: false,
         },
         fullName,
