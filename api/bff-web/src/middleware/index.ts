7f7da11d691655aec2277828c0b61097431170af
uma-uc <uma.apple123@gmail.com>
2025-09-14 21:52:35 -0700

    feat: wip

---
+++ api/bff-web/src/middleware/index.ts
@@ -0,0 +1,6 @@
+import type { FastifyInstance } from "fastify"
+
+export const setupMiddleware = async (_app: FastifyInstance) => {
+  // TODO: Setup middleware for bff-web
+  // This might include CORS, authentication, logging, etc.
+}
